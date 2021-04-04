0acc1e31fde3a996558634678314a9272c840d20
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-04 19:23:35 +0700

    Create list_uname.py

---
+++ userbot/modules/list_uname.py
@@ -0,0 +1,23 @@
+from telethon import events, functions, types
+from userbot.utils.tools import admin_cmd
+from userbot import CMD_HELP, bot
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
+
+@register(pattern=r"^\.xlist")
+async def mine(event):
+    """ For .reserved command, get a list of your reserved usernames. """
+    result = await bot(GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
+    await event.edit(output_str)
+
+
+
+CMD_HELP.update(
+    {
+        "ListUsernames": "**Plugin : **`listusername`\
+    \n\n**Syntax : **`.xlist`\
+    \n**Function : **this plugin give you your all channel and groups usernamen"
+    }
+)
