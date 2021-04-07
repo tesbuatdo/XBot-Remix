bba3a0cc6a1e93bc2e95000d33ec5ba15177b5e0
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-07 12:11:24 +0000

    pylint: auto fixes
    Signed-off-by: ximfine <xbotprojects@gmail.com>

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -13,7 +13,9 @@ from telethon import TelegramClient
 from telethon.events import NewMessage
 from telethon import functions, types, events
 from userbot.events import register
-import requests, os ,re
+import requests
+import os
+import re
 import PIL
 import cv2
 import random
@@ -28,9 +30,16 @@ from telethon.tl.types import DocumentAttributeFilename, MessageMediaPhoto
 
 from userbot import bot
 
+
 def transcode(filename):
-    ffmpeg.input(filename).output("input.raw", format='s16le', acodec='pcm_s16le', ac=1, ar='48000').overwrite_output().run() 
+    ffmpeg.input(filename).output(
+        "input.raw",
+        format='s16le',
+        acodec='pcm_s16le',
+        ac=1,
+        ar='48000').overwrite_output().run()
     os.remove(filename)
+
 
 vc = GroupCall(bot, input_filename="input.raw", play_on_repeat=True)
 
@@ -41,6 +50,7 @@ playing = False  # Tells if something is playing or not
 chat_joined = False  # Tell if chat is joined or not
 
 path = "./downloads/"
+
 
 @register(outgoing=True, pattern=r"^\.play$")
 async def vcg(event):
@@ -56,8 +66,8 @@ async def vcg(event):
     await event.edit("Memutar Music...")
     await asyncio.sleep(600)
     os.remove(input.raw)
-    
-    
+
+
 @register(outgoing=True, pattern=r"^\.joinvc$")
 async def joinvc(event):
     global chat_joined
@@ -72,6 +82,7 @@ async def joinvc(event):
         await asyncio.sleep(10)
         await event.delete()
 
+
 @register(outgoing=True, pattern=r"^\.leavevc$")
 async def leavevc(event):
     global chat_joined
@@ -82,4 +93,3 @@ async def leavevc(event):
     await event.edit("__**Left The Voice Chat.**__")
     await asyncio.sleep(10)
     await event.delete()
-
