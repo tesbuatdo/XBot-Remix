6f6c750ea9dc9e0f868a5a8cce5dc1a16fe86ead
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-07 22:00:26 +0700

    Update callsmusic.py

--- userbot/callsmusic/callsmusic.py
+++ userbot/callsmusic/callsmusic.py
@@ -1,9 +1,12 @@
-from userbot import bot
+from userbot import bot, API_KEY, API_HASH, STRING_SESSION
+from telethon.sync import TelegramClient
 from pytgcalls import PyTgCalls
 
-from userbot.callsmusic import queues
+from .callsmusic import queues
 
-pytgcalls = PyTgCalls(bot)
+jemboed = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+pytgcalls = PyTgCalls(jemboed)
+
 
 
 @pytgcalls.on_stream_end()
