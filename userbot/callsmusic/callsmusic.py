f90cf735cb6595e986925fd9b4e69ec38ebe2190
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-07 20:52:38 +0700

    Create callsmusic.py

---
+++ userbot/callsmusic/callsmusic.py
@@ -0,0 +1,21 @@
+from userbot import bot
+from pytgcalls import PyTgCalls
+
+from userbot.callsmusic import queues
+
+pytgcalls = PyTgCalls(bot)
+
+
+@pytgcalls.on_stream_end()
+def on_stream_end(chat_id: int) -> None:
+    queues.task_done(chat_id)
+
+    if queues.is_empty(chat_id):
+        pytgcalls.leave_group_call(chat_id)
+    else:
+        pytgcalls.change_stream(
+            chat_id, queues.get(chat_id)["file_path"]
+        )
+
+
+run = pytgcalls.run
