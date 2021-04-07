c76d1b322acacfa355817e9074bf85b7345de43c
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-07 22:41:24 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -2,33 +2,45 @@ from __future__ import unicode_literals
 import os
 from os import path
 import ffmpeg
-from typing import Union
+from pytgcalls import GroupCall
+from Python_ARQ import ARQ
 from userbot.events import register
-from userbot import bot, converter, callsmusic
+from userbot import bot
 from telethon.tl.types import DocumentAttributeAudio as Audio
 
 
-def get_file_name(audio: Union[Audio]):
-    return f'{audio.file_unique_id}.{audio.file_name.split(".")[-1] if not isinstance(audio) else "ogg"}'
+def transcode(filename):
+    ffmpeg.input(filename).output(
+        "input.raw",
+        format='s16le',
+        acodec='pcm_s16le',
+        ac=1,
+        ar='48000').overwrite_output().run()
+    os.remove(filename)
+
+    return text[offset:offset + length]
+
+ARQ_API = "http://35.240.133.234:8000"
+
+arq = ARQ(ARQ_API)
+vc = GroupCall(bot, input_filename="input.raw", play_on_repeat=True)
+
+playing = False  # Tells if something is playing or not
+chat_joined = False  # Tell if chat is joined or not
 
 
 @register(outgoing=True, pattern=r"^\.play")
-async def xvcg(event):
+async def vcg(event):
     if event.fwd_from:
         return
-    audio = await event.get_reply_message()
-    if not (audio and (audio.media)):
+    global playing
+    ureply = await event.get_reply_message()
+    if not (ureply and (ureply.media)):
         await event.edit("`Reply to any media`")
         return
-    file_name = get_file_name(audio)
-    file_path = await converter.convert(
-            (await bot.download_media(file_name))
-            if not path.isfile(path.join("downloads", file_name)) else file_name
-        )
     await event.edit("Downloading Music....")
-    if event.chat.id in callsmusic.pytgcalls.active_calls:
-        await event.edit(f"Queued at position {await callsmusic.queues.put(event.chat.id, file_path=file_path)}!")
-    else:
-        callsmusic.pytgcalls.join_group_call(event.chat.id, file_path)
-        await event.edit("Memutar Music...")
-
+    song = await event.client.download_media(ureply)
+    await event.edit("Transcode...")
+    transcode(song)
+    playing = False  # pylint:disable=E0602
+    await event.edit("Memutar Music...")
