6547470be7ed2ce94d7ba52d4f3fafaff33cb5f6
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-10 14:35:34 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -276,7 +276,7 @@ def paginate_help(page_number, loaded_modules, prefix):
         custom.Button.inline("{} {}".format("☠️", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
-    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
