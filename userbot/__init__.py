3b3e3385502a151e3340874293be0ed1c6512890
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-10 13:49:03 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -390,7 +390,7 @@ with bot:
 
         @tgbot.on(events.CallbackQuery(data=b'close'))
         async def close(event):
-            await buttons=Button.clear()
+            await event.client.send_message(event.chat_id, 'Closed!', buttons=Button.clear())
 
             
         @tgbot.on(
