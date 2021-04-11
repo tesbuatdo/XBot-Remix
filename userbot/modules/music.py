a203ace3b0f613e237c3b5297039601ee8dbc231
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-11 21:18:15 +0700

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -17,7 +17,14 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 from youtube_search import YoutubeSearch
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (DownloadError, ContentTooShortError,
 
+                              ExtractorError, GeoRestrictedError,
+                              MaxDownloadsReached, PostProcessingError,
+                              UnavailableVideoError, XAttrMetadataError)
+
+from youtubesearchpython import SearchVideos
 from userbot import (
     CMD_HELP,
     DEEZER_ARL_TOKEN,
@@ -105,7 +112,7 @@ async def song(v_url):
     url = v_url.pattern_match.group(1)
     if not url:
         return await rkp.edit("`Error \nusage song <song name>`")
-    search = YoutubeSearch(url, offset=1, mode="json", max_results=1)
+    search = SearchVideos(url, offset=1, mode="json", max_results=1)
     test = search.result()
     p = json.loads(test)
     q = p.get('search_result')
