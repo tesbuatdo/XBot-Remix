4949921542df34551695a3924137067c33c32229
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-12 19:24:47 +0700

    Update __init__.py

--- userbot/modules/sql_helper/__init__.py
+++ userbot/modules/sql_helper/__init__.py
@@ -1,14 +1,16 @@
-from pymongo import MongoClient, collection
-from userbot import MONGO_URI, LOGGER
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from userbot import DB_URI
+
+BASE = declarative_base()
 
 
-LOGGER.info("Connecting to MongoDB")
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-DB_CLIENT = MongoClient(MONGO_URI)
 
-_DB = DB_CLIENT["xbot"]
-
-
-def get_collection(name: str) -> collection:
-    """Get the collection from database."""
-    return _DB[name]
+SESSION = start()
