a4e27968a2ffe394e402a7328255077897f6802a
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-12 13:00:42 +0700

    Create repo.py

---
+++ userbot/modules/asissten/repo.py
@@ -0,0 +1,34 @@
+from random import randint
+from time import sleep
+from os import execl
+import asyncio
+import sys
+import os
+import io
+import sys
+from telethon import events, Button
+from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, tgbot
+from userbot.events import register
+from userbot.utils import time_formatter
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import re
+from PIL import Image
+
+rtext = """
+🔥 XBOT REMIX USERBOT 🔥
+
+  Running with telethon modules,
+
+• XBOT version: X01
+• License: [Raphielscape](https://github.com/ximfine/XBot-Remix/blob/alpha/LICENSE)
+
+Thanks for using repo
+"""
+
+@register(outgoing=True, pattern="!repo")
+async def repo(repo):
+    await tgbot.send_message(repo.chat_id, caption=rtext, 
+                             buttons=[[Button.url(text="GITHUB REPO",
+                                               url="https://github.com/ximfine/XBot-Remix")]]
