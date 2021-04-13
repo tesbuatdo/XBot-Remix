9227ddf38b3b0a9b2011fa7e2f28efa478e969c3
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-13 04:07:14 +0000

    pylint: auto fixes
    Signed-off-by: ximfine <xbotprojects@gmail.com>

--- userbot/modules/core.py
+++ userbot/modules/core.py
@@ -1,22 +1,13 @@
 import asyncio
-import datetime
 import importlib
-import inspect
 import logging
-import math
-import re
 import sys
-import time
-import traceback
 from pathlib import Path
-from time import gmtime, strftime
 
-from telethon import events
-from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
-from userbot import CMD_HELP, LOAD_PLUG, LOGS, bot
+from userbot import CMD_HELP, LOGS, bot
 
 DELETE_TIMEOUT = 5
+
 
 def load_module(shortname):
     if shortname.startswith("__"):
@@ -29,7 +20,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         LOGS.info("Successfully imported " + shortname)
     else:
-        
+
         path = Path(f"userbot/modules/{shortname}.py")
         name = "userbot.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
@@ -38,18 +29,18 @@ def load_module(shortname):
         mod.LOGS = LOGS
         mod._format = _format
         mod.tgbot = bot.tgbot
-        
+
         mod.CMD_HELP = CMD_HELP
         mod.reply_id = reply_id
-        
+
         mod.media_type = media_type
-        
+
         mod.install_pip = install_pip
         mod.parse_pre = _format.parse_pre
 
         mod.logger = logging.getLogger(shortname)
         # support for uniborg
-        
+
         # support for paperplaneextended
         sys.modules["userbot.events"] = userbot.utils
         spec.loader.exec_module(mod)
