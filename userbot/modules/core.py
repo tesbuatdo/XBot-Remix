462cfbbd480818e90d4779aa6940c7dac9dae112
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-13 04:31:40 +0000

    pylint: auto fixes
    Signed-off-by: ximfine <xbotprojects@gmail.com>

--- userbot/modules/core.py
+++ userbot/modules/core.py
@@ -5,7 +5,7 @@ import sys
 import os
 from pathlib import Path
 
-from userbot import CMD_HELP, LOGS, bot, tgbot
+from userbot import CMD_HELP, LOGS, bot
 from userbot.events import register
 
 DELETE_TIMEOUT = 5
