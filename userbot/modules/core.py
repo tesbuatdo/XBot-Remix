343f733e78e7c5532c2c41ff959f829d8e4ffe0c
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-13 11:06:23 +0700

    Update core.py

--- userbot/modules/core.py
+++ userbot/modules/core.py
@@ -1,9 +1,61 @@
-from pathlib import Path
 import asyncio
-import os
-from userbot.events import register
+import datetime
+import importlib
+import inspect
+import logging
+import math
+import re
+import sys
+import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
+
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+from userbot import CMD_HELP, LOAD_PLUG, LOGS, bot
 
 DELETE_TIMEOUT = 5
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        path = Path(f"userbot/modules/{shortname}.py")
+        name = "userbot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Successfully imported " + shortname)
+    else:
+        
+        path = Path(f"userbot/modules/{shortname}.py")
+        name = "userbot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = bot
+        mod.LOGS = LOGS
+        mod._format = _format
+        mod.tgbot = bot.tgbot
+        
+        mod.CMD_HELP = CMD_HELP
+        mod.reply_id = reply_id
+        
+        mod.media_type = media_type
+        
+        mod.install_pip = install_pip
+        mod.parse_pre = _format.parse_pre
+
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        
+        # support for paperplaneextended
+        sys.modules["userbot.events"] = userbot.utils
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["userbot.modules." + shortname] = mod
+        LOGS.info("Successfully imported " + shortname)
 
 
 @register(outgoing=True, pattern=r"^\.install$")
