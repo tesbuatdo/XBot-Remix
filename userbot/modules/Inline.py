cf4137d29dd1fef319a807ae3150f697295efb09
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-14 19:40:37 +0700

    Update Inline.py

--- userbot/modules/Inline.py
+++ userbot/modules/Inline.py
@@ -1,5 +1,7 @@
-from userbot import tgbot
-
+from userbot import tgbot, StartTime
+import speedtest
+import time
+from datetime import datetime
 from telethon import Button, events
 import logging
 
@@ -25,3 +27,77 @@ async def xrepo(repo):
     await tgbot.send_message(repo.chat_id, rtext,
                              buttons=[[Button.url(text="GITHUB REPO",
                                                   url="https://github.com/ximfine/XBot-Remix")]])
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+def speed_convert(size):
+    """
+    Hi human, you can't read bytes?
+    """
+    power = 2**10
+    zero = 0
+    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
+
+@tgbot.on(events.NewMessage(pattern="!speed"))
+async def spd(event):
+    await event.reply("`Test Speed Internet connection` ⚡")
+    start = datetime.now()
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    response = s.results.dict()
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    client_infos = response.get("client")
+    i_s_p = client_infos.get("isp")
+    i_s_p_rating = client_infos.get("isprating")
+    response = s.results.share()
+    speedtest_image = response
+    output = (f"**SpeedTest** completed in {ms}ms\n\n"
+              f"`•Download: {speed_convert(download_speed)}\n`"
+              f"`•Upload: {speed_convert(upload_speed)}\n`"
+              f"`•Ping: {ping_time}\n`"
+              f"`•ISP: {i_s_p}\n`"
+              f"`•ISP Rating: {i_s_p_rating}\n\n`"
+              "**POWERED BY XBOT REMIX 🔥**")
+    await tgbot.send_file(
+        event.chat_id,
+        speedtest_image,
+        caption=output,
+        force_document=False,
+        allow_cache=False
+    )
+    await event.delete()
