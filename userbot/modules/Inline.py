60d113ce475f79eaf3fa897ae9999e1335f5bcaa
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-14 19:22:05 +0700

    Update Inline.py

--- userbot/modules/Inline.py
+++ userbot/modules/Inline.py
@@ -1,7 +1,8 @@
-from userbot import tgbot
+from userbot import tgbot, bot
 from telethon import Button, events
 import logging
-
+import random
+import re
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
@@ -25,3 +26,28 @@ async def xrepo(repo):
     await tgbot.send_message(repo.chat_id, rtext,
                              buttons=[[Button.url(text="GITHUB REPO",
                                                   url="https://github.com/ximfine/XBot-Remix")]])
+
+
+
+xrepo = ("Klik here", buttons=[[Button.url(text="GITHUB REPO",
+                                                  url="https://github.com/ximfine/XBot-Remix")]])
+
+@register(outgoing=True, pattern=r"^\.xrepo")
+async def yardim(event):
+    tgbotusername = BOT_USERNAME
+    if tgbotusername and BOT_TOKEN:
+        try:
+            results = await event.client.inline_query(tgbotusername, xrepo)
+        except BotInlineDisabledError:
+            return await event.edit(
+                "`Bot can't be used in inline mode.\nMake sure to turn on inline mode!`"
+            )
+        await results[0].click(
+            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+        )
+        await event.delete()
+    else:
+        return await event.edit(
+            "`The bot doesn't work! Please set the Bot Token and Username correctly.`"
+            "\n`The module has been stopped.`"
+        )
