79ea9d70756a1de7cbef9259717f25a5979de460
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-24 22:49:47 +0700

    Update hash.py

--- userbot/modules/hash.py
+++ userbot/modules/hash.py
@@ -8,10 +8,12 @@ from subprocess import PIPE
 from subprocess import run as runapp
 
 import pybase64
-
+from telethon import events
+import io
 from userbot import CMD_HELP
 from userbot.events import register
 
+MAX_MESSAGE_SIZE_LIMIT  = 4095
 
 @register(outgoing=True, pattern=r"^\.hash (.*)")
 async def gethash(hash_q):
@@ -73,13 +75,32 @@ async def endecrypt(query):
         await query.reply("Decoded: `" + lething[:-1] + "`")
 
 
-@register(outgoing=True, pattern="^\\.json")
+@register(outgoing=True, pattern="^.json$")
 async def _(event):
     if event.fwd_from:
         return
-    catevent = await event.get_reply_message() if event.reply_to_msg_id else event
-    the_real_message = catevent.stringify()
-    await event.edit(the_real_message, parse_mode=parse_pre)
+    the_real_message = None
+    reply_to_id = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        the_real_message = previous_message.stringify()
+        reply_to_id = event.reply_to_msg_id
+    else:
+        the_real_message = event.stringify()
+        reply_to_id = event.message.id
+    if len(the_real_message) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(the_real_message)) as out_file:
+            out_file.name = "json.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit("`{}`".format(the_real_message))
 
 CMD_HELP.update(
     {
