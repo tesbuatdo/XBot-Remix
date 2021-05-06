2cb7036f43499cdd12569f21e828aaa4a60752b2
ImFine <79558474+ximfine@users.noreply.github.com>
2021-05-06 14:05:52 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -7,78 +7,84 @@ from aiohttp import web
 from aiohttp.http_websocket import WSMsgType
 
 from telethon.tl.functions.channels import GetFullChannelRequest
-from telethon.tl.functions.phone import GetGroupCallRequest
-from telethon.tl.functions.phone import JoinGroupCallRequest
+from telethon.tl.functions.phone import (
+    GetGroupCallRequest,
+    JoinGroupCallRequest,
+    LeaveGroupCallRequest,
+)
 from telethon.tl.types import DataJSON
 
-
-async def get_entity(chat):
-    try:
-        return await bot.get_input_entity(chat['id'])
-    except ValueError:
-        if 'username' in chat:
-            return await bot.get_entity(chat['username'])
-        raise
-
-
-@register(outgoing=True, pattern=r"^\.joinvcg")
+@register(outgoing=True, pattern=r"^\.joinvc")
 async def join_call(data):
-    chat = await get_entity(data['chat'])
-    full_chat = await bot(GetFullChannelRequest(chat))
-    call = await bot(GetGroupCallRequest(full_chat.full_chat.call))
+        try:
+            chat = await get_entity(data["chat"])
+        except ValueError:
+            stree = (await bot.get_me()).first_name
+            return await bot.send_message(
+                data["chat"]["id"], f"`Please add {stree} in this group.`"
+            )
+        except Exception as ex:
+            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
+        try:
+            full_chat = await bot(GetFullChannelRequest(chat))
+        except ValueError:
+            stree = (await bot.get_me()).first_name
+            return await bot.send_message(
+                data["chat"]["id"], f"`Please add {stree} in this group.`"
+            )
+        except Exception as ex:
+            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
+        try:
+            call = await bot(GetGroupCallRequest(full_chat.full_chat.call))
+        except BaseException:
+            call = None
+        if not call:
+            return await bot.send_message(
+                data["chat"]["id"],
+                "`I can't access voice chat.`",
+            )
 
-    result = await bot(
-        JoinGroupCallRequest(
-            call=call.call,
-            muted=False,
-            params=DataJSON(
-                data=json.dumps({
-                    'ufrag': data['ufrag'],
-                    'pwd': data['pwd'],
-                    'fingerprints': [{
-                        'hash': data['hash'],
-                        'setup': data['setup'],
-                        'fingerprint': data['fingerprint'],
-                    }],
-                    'ssrc': data['source'],
-                }),
-            ),
-        ),
-    )
+        try:
+            result = await bot(
+                JoinGroupCallRequest(
+                    call=call.call,
+                    muted=False,
+                    join_as="me",
+                    params=DataJSON(
+                        data=json.dumps(
+                            {
+                                "ufrag": data["ufrag"],
+                                "pwd": data["pwd"],
+                                "fingerprints": [
+                                    {
+                                        "hash": data["hash"],
+                                        "setup": data["setup"],
+                                        "fingerprint": data["fingerprint"],
+                                    },
+                                ],
+                                "ssrc": data["source"],
+                            },
+                        ),
+                    ),
+                ),
+            )
+            await bot.send_message(
+                data, f"`Joined Voice Chat in {(await bot.get_entity(data['chat']['id'])).title}`",
+            )
+        except Exception as ex:
+            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
 
-    transport = json.loads(result.updates[0].call.params.data)['transport']
+        transport = json.loads(result.updates[0].call.params.data)["transport"]
 
-    return {
-        '_': 'get_join',
-        'data': {
-            'chat_id': data['chat']['id'],
-            'transport': {
-                'ufrag': transport['ufrag'],
-                'pwd': transport['pwd'],
-                'fingerprints': transport['fingerprints'],
-                'candidates': transport['candidates'],
+        return {
+            "_": "get_join",
+            "data": {
+                "chat_id": data["chat"]["id"],
+                "transport": {
+                    "ufrag": transport["ufrag"],
+                    "pwd": transport["pwd"],
+                    "fingerprints": transport["fingerprints"],
+                    "candidates": transport["candidates"],
+                },
             },
-        },
-    }
-
-
-async def websocket_handler(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    async for msg in ws:
-        if msg.type == WSMsgType.TEXT:
-            try:
-                data = json.loads(msg.data)
-            except JSONDecodeError:
-                await ws.close()
-                break
-
-            response = None
-            if data['_'] == 'join':
-                response = await join_call(data['data'])
-
-            if response is not None:
-                await ws.send_json(response)
-
-    return ws
+        }
