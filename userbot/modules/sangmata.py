72d2578236b83416f0275ad23e31b74d2bd5aadd
ImFine <79558474+ximfine@users.noreply.github.com>
2021-05-06 18:24:46 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -1,77 +1,112 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Port to userbot by @MoveAngel
+"""
+By :- @Zero_cool7870
+ported char, airing and manga by @sandy1709 and @mrconfused
+"""
+
+import asyncio
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+import datetime
+import os
+import re
+import time
+import urllib.request
+from random import choice
+import requests
+from telethon.tl.tlobject import TLObject
+from telethon.tl.types import MessageEntityPre
+from telethon.utils import add_surrogate
+
 from userbot.events import register
-from asyncio.exceptions import TimeoutError
+from userbot import bot, CMD_HELP
 
+def parse_pre(text):
+    text = text.strip()
+    return (
+        text,
+        [MessageEntityPre(offset=0, length=len(add_surrogate(text)), language="")],
+    )
 
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Port to userbot by @MoveAngel
+async def sanga_seperator(sanga_list):
+    for i in sanga_list:
+        if i.startswith("🔗"):
+            sanga_list.remove(i)
+    s = 0
+    for i in sanga_list:
+        if i.startswith("Username History"):
+            break
+        s += 1
+    usernames = sanga_list[s:]
+    names = sanga_list[:s]
+    return names, usernames
 
-
-@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
-async def lastname(steal):
-    if steal.fwd_from:
+@register(outgoing=True, pattern=r"^\.(?:sg|sgu)($| (.*))")
+async def _(event):
+    if event.fwd_from:
         return
-    if not steal.reply_to_msg_id:
-        await steal.edit("```Reply to any user message.```")
-        return
-    message = await steal.get_reply_message()
-    chat = "@SangMataInfo_bot"
-    user_id = message.sender.id
-    id = f"/search_id {user_id}"
-    if message.sender.bot:
-        await steal.edit("```Reply to actual users message.```")
-        return
-    await steal.edit("```Sit tight while I steal some data from NASA```")
-    try:
-        async with bot.conversation(chat) as conv:
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    reply_message = await event.get_reply_message()
+    if not input_str and not reply_message:
+        await event.edit("`reply to  user's text message to get name/username history or give userid/username`",
+        )
+    if input_str:
+        try:
+            uid = int(input_str)
+        except ValueError:
             try:
-                msg = await conv.send_message(id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await steal.reply(
-                    "```Please unblock @sangmatainfo_bot and try again```"
+                u = await event.client.get_entity(input_str)
+            except ValueError:
+                await event.edit("`Give userid or username to find name history`"
                 )
-                return
-            if r.text.startswith("Name"):
-                respond = await conv.get_response()
-                await steal.edit(f"{r.message}")
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
-                )
-                return
-            if response.text.startswith("No records") or r.text.startswith(
-                "No records"
-            ):
-                await steal.edit("```No records found for this user```")
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id]
-                )
-                return
+            uid = u.id
+    else:
+        uid = reply_message.sender_id
+    chat = "@SangMataInfo_bot"
+    catevent = await event.edit("`Processing...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message(f"/search_id {uid}")
+        except YouBlockedUserError:
+            await event.edit("`unblock @Sangmatainfo_bot and then try`")
+        responses = []
+        while True:
+            try:
+                response = await conv.get_response(timeout=2)
+            except asyncio.TimeoutError:
+                break
+            responses.append(response.text)
+        await event.client.send_read_acknowledge(conv.chat_id)
+    if not responses:
+        await event.edit("`bot can't fetch results`")
+    if "No records found" in responses:
+        await event.edit("`The user doesn't have any record`")
+    names, usernames = await sanga_seperator(responses)
+    cmd = event.pattern_match.group(1)
+    if cmd == "sg":
+        sandy = None
+        for i in names:
+            if sandy:
+                await event.reply(i, parse_mode=parse_pre)
             else:
-                respond = await conv.get_response()
-                await steal.edit(f"{response.message}")
-            await steal.client.delete_messages(
-                conv.chat_id, [msg.id, r.id, response.id, respond.id]
-            )
-    except TimeoutError:
-        return await steal.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
+                sandy = True
+                await catevent.edit(i, parse_mode=parse_pre)
+    elif cmd == "sgu":
+        sandy = None
+        for i in usernames:
+            if sandy:
+                await event.reply(i, parse_mode=parse_pre)
+            else:
+                sandy = True
+                await catevent.edit(i, parse_mode=parse_pre)
 
 
-CMD_HELP.update({
-    "sangmata":
-        "`.sg`\
-          \nUsage: Steal ur or friend name."
-})
+CMD_HELP.update(
+    {
+        "sangmata": "**Plugin : **`sangmata`\
+    \n\n**Syntax : **`.sg <username/userid/reply>`\
+    \n**Function : **__Shows you the previous name history of user.__\
+    \n\n**Syntax : **`.sgu <username/userid/reply>`\
+    \n**Function : **__Shows you the previous username history of user.__\
+    "
+    }
+)
