a63bfaadc18e35ea99512901deeb1502bf6aad56
ImFine <79558474+ximfine@users.noreply.github.com>
2021-05-08 07:06:58 +0000

    pylint: auto fixes
    Signed-off-by: ximfine <xbotprojects@gmail.com>

--- userbot/modules/users_sql.py
+++ userbot/modules/users_sql.py
@@ -1,8 +1,9 @@
 import threading
 
-from sqlalchemy import Column, Integer, UnicodeText, String, ForeignKey, UniqueConstraint, func
+from sqlalchemy import Column, ForeignKey, Integer, String, UnicodeText, UniqueConstraint
 
 from userbot.modules.sql_helper import BASE, SESSION
+
 
 class Users(BASE):
     __tablename__ = "users"
@@ -44,15 +45,20 @@ class ChatMembers(BASE):
                              onupdate="CASCADE",
                              ondelete="CASCADE"),
                   nullable=False)
-    __table_args__ = (UniqueConstraint('chat', 'user', name='_chat_members_uc'),)
+    __table_args__ = (
+        UniqueConstraint(
+            'chat',
+            'user',
+            name='_chat_members_uc'),
+    )
 
     def __init__(self, chat, user):
         self.chat = chat
         self.user = user
 
     def __repr__(self):
-        return "<Chat user {} ({}) in chat {} ({})>".format(self.user.username, self.user.user_id,
-                                                            self.chat.chat_name, self.chat.chat_id)
+        return "<Chat user {} ({}) in chat {} ({})>".format(
+            self.user.username, self.user.user_id, self.chat.chat_name, self.chat.chat_id)
 
 
 Users.__table__.create(checkfirst=True)
@@ -60,6 +66,7 @@ Chats.__table__.create(checkfirst=True)
 ChatMembers.__table__.create(checkfirst=True)
 
 INSERTION_LOCK = threading.RLock()
+
 
 def get_all_chats():
     try:
